d32fa07876b07409a1ed374a207bf90ea4ac83d2
Schtoo <mgqobhoz@gmail.com>
2018-07-25 12:16:02 +0200

    deployed folder, now deploying file

---
+++ greet_schtoo.js
@@ -0,0 +1,11 @@
+const figlet = require('figlet');
+const greet = require('./greet');
+
+figlet('Hello Schtoo', function(err, data) {
+   if(err) {
+      console.log('Something went wrong....');
+      console.log(err);
+      return;
+     }
+    console.log(data)
+ });
