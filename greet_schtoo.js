940498727eb6caa09a4ffdeac500a82614f414cb
Schtoo <mgqobhoz@gmail.com>
2018-07-25 12:54:39 +0200

    deplying the right files

--- greet_schtoo.js
+++ greet_schtoo.js
@@ -2,10 +2,10 @@ const figlet = require('figlet');
 const greet = require('./greet');
 
 figlet('Hello Schtoo', function(err, data) {
-   if(err) {
-      console.log('Something went wrong....');
-      console.log(err);
-      return;
-     }
-    console.log(data)
- });
+  if (err) {
+    console.log('Something went wrong..');
+    console.log(err);
+    return;
+  }
+  console.log(data)
+});
